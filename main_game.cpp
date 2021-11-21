697b16aefa0e67de032109a53964ac557b24ed26
Redwane <redwanehammas@gmail.com>
2021-11-21 13:15:59 +0100

    Add refresh console functionality

--- main_game.cpp
+++ main_game.cpp
@@ -2,12 +2,26 @@
 #include <iostream>
 using namespace std;
 
+// TODO write documentation
+void refreshConsole() {
+#if defined _WIN32 // preprocessor directive
+    //clrscr(); // include conio.h
+    system("cls");
+#elif defined (__linux__) || defined(__gnu_linux__) || defined(__LINUX__) || defined(__APPLE__)
+    // system("clear");
+    cout << "\x1B[2J\x1B[H";
+#endif
+}
+
 int main() {
+    refreshConsole();
     timeInit();
 
     Plateau plateau = plateauInitial();
     cout << dessine(plateau);
 
+    // ISSUE not estTermine(plateau) or not estGagnant(plateau) -> returns true if loser
+    // ISSUE game continues even if estGangnant(plateau)
     while(not estTermine(plateau) or not estGagnant(plateau)) {
         string dir;
         cout << "Entrer dir : ";
@@ -24,7 +38,7 @@ int main() {
         if (dir == "k") {
             plateau = deplacement(plateau, BAS);
         }
+        refreshConsole();
         cout << dessine(plateau);
     }
-
 }
